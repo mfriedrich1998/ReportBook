c7e76c40663dc51f56451a9991640d221b615a84
Michelle Friedrich <mfriedrich@rt.ipnt.de>
2019-10-22 09:55:53 +0200

    Jasper stuff

--- src/main/java/backend/PDF/PDFGenerator.java
+++ src/main/java/backend/PDF/PDFGenerator.java
@@ -1,6 +1,8 @@
 package backend.PDF;
 
 
+import backend.I18N.I18N;
+import backend.saveinstance.SaveInstance;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.util.JRSaver;
 
@@ -13,6 +15,47 @@ import java.util.Map;
 
 
 public class PDFGenerator {
+
+    private SaveInstance instance = SaveInstance.getInstance();
+
+    String firstDate = instance.getFromDate();
+
+    String toDate = instance.getToDate();
+
+    String subject = instance.getSchoolSubject();
+
+    String secSubject = instance.getSecondSchoolSubject();
+
+    String schoolinput = instance.getSubjectInput();
+
+    String secSchoolinput = instance.getSecondSubjectInput();
+
+    int number = instance.getReportBookNumber();
+
+    String activities = instance.getActivitiesText();
+
+    String instructions = instance.getInstructionsText();
+
+    String name = I18N.get("key37");
+
+    String department = I18N.get("key44");
+
+    String rbNumber = I18N.get("key38");
+
+    String traineeSignature = I18N.get("key42");
+
+    String mentorSignature = I18N.get("key43");
+
+    String schoolSubjects = I18N.get("key41");
+
+    String profActivities = I18N.get("key39");
+
+    String instruction = I18N.get("key40");
+
+    String from = I18N.get("key45");
+
+    String to = I18N.get("key46");
+
 
 
     public static void main(String[] args) {
@@ -35,13 +78,21 @@ public class PDFGenerator {
             JasperReport jasperReport = JasperCompileManager.compileReport(resource);
 
             Map<String, Object> params = new HashMap<String, Object>();
-            params.put("RB_NUMBER", "Report Book Number ");
-            params.put("DEPARTMENT", "Department");
+            params.put("RB_NUMBER", rbNumber + number );
+            params.put("DEPARTMENT", department);
             params.put("DEPARTMENT_NAME", "SustainHub");
-            params.put("TRAINEES_NAME", "Trainees name");
+            params.put("TRAINEES_NAME", name);
             params.put("NAME", "Michelle Friedrich");
-            params.put("SIGNATURE_TRAINEE", "Signature trainee");
-            params.put("SIGNATURE_MENTOR", "Signature mentor");
+            params.put("SIGNATURE_TRAINEE", traineeSignature);
+            params.put("SIGNATURE_MENTOR", mentorSignature);
+            params.put("SCHOOL_SUBJECTS", schoolSubjects);
+            params.put("DAY_1", subject + " " + schoolinput);
+            params.put("DAY_2", secSubject + " " + secSchoolinput);
+            params.put("ACTIVITIES", activities);
+            params.put("PROFESSIONAL_ACTIVITIES", profActivities);
+            params.put("INSTRUCTIONS_HEADER", instruction);
+            params.put("INSTRUCTIONS", instructions);
+            params.put("FROM_TODATE", from + firstDate + to + toDate);
 
             InputStream subreport1 = PDFGenerator.class.getResourceAsStream("../../reports/ProfActivities.jrxml");
 
